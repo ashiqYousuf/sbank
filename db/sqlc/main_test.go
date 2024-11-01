e0a9ff30078ce3f98b5ddce3e45fc13ac88cc21a
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-11-01 09:58:16 +0530

    Added Money transfer feature, avoid deadlocks

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,15 +15,17 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	// entry point of all unit tests inside one specific go package
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
