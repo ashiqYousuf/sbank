93f23a9a9a54f8fd0828c84555830b31d879b823
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-11-08 12:14:38 +0530

    Added viper to load appn config from env vars & files

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,12 +6,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/ashiqYousuf/sbank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -20,8 +16,12 @@ var testDB *sql.DB
 func TestMain(m *testing.M) {
 	// TestMain is the main entry point of all the unit tests
 	// inside one single golang package
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
