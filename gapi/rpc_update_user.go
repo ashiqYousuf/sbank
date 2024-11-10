2064e1a8fde47d46058041452e48f19a3e3138e2
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-11-10 10:30:38 +0530

    Added authentication &authorization to the gRPC apis

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -16,6 +16,16 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	// Authorize the request
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
+	}
+
 	violations := validateUpdateUserRequest(req)
 	if violations != nil {
 		return nil, invalidArgumentError(violations)
