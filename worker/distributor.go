fa25c22b7e2f97f791ac6b556a53f14222dd5ed0
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-11-10 18:41:09 +0530

    reinit

---
+++ worker/distributor.go
@@ -0,0 +1,32 @@
+package worker
+
+import (
+	"context"
+
+	"github.com/hibiken/asynq"
+)
+
+/*
+	Code from tech-school
+	No need to re-implement it here
+*/
+
+type TaskDistributor interface {
+	DistributeTaskSendVerifyEmail(
+		ctx context.Context,
+		payload *PayloadSendVerifyEmail,
+		opts ...asynq.Option,
+	) error
+}
+
+// Implements TaskDistributor interface
+type RedisTaskDistributor struct {
+	client *asynq.Client
+}
+
+func NewRedisTaskDistributor(redisOpt asynq.RedisClientOpt) TaskDistributor {
+	client := asynq.NewClient(redisOpt)
+	return &RedisTaskDistributor{
+		client: client,
+	}
+}
